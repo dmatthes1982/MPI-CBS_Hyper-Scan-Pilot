1586b68e2fcb5719ba116fab91ab01c8d40da1ff
Daniel Matthes <dmatthes@cbs.mpg.de>
2017-06-15 11:16:32 +0200

    Funktion HSP_importSingleDataset(…) – Funktion umgebaut, dass die Trials nun anhand der Stimuli erstellt werden und das bei aufeinanderfolgenden identischen Stimuli immer nur der Letzte ausgewertet wird.

--- HSP_importSingleDataset.m
+++ HSP_importSingleDataset.m
@@ -1,17 +1,29 @@
 function [ data ] = HSP_importSingleDataset( headerfile )
 
-hdr                       = ft_read_header( headerfile);
-
 cfg                       = [];
 cfg.dataset               = headerfile;
 cfg.trialfun              = 'ft_trialfun_general';
-cfg.trialdef.eventtype    = 'New Segment';
+cfg.trialdef.eventtype    = 'Stimulus';
+cfg.trialdef.eventvalue   = {'S160', 'S150', 'S152', 'S149', 'S228', 'S179', 'S138', 'S159', 'S180', 'S169', 'S170', 'S181'};
+%cfg.trialdef.eventvalue   = {'S32', 'S22', 'S24', 'S21', 'S100', 'S51', 'S10', 'S31', 'S52', 'S41', 'S42', 'S53'};
+cfg.trialdef.prestim      = 0;
+cfg.trialdef.poststim     = 180;
 cfg.showcallinfo          = 'no';
 
 cfg_seg = ft_definetrial(cfg);
+falseTrig = [];
 
-cfg_seg.trl(1:end-1, 2) = cfg_seg.trl(2:end, 1)-1;
-cfg_seg.trl(end, 2) = hdr.nSamples;
+for i=1:1:size(cfg_seg.trl,1)-1
+  if cfg_seg.trl(i,4) == cfg_seg.trl(i+1,4)
+    falseTrig = [falseTrig, i];
+  end
+end
+
+if ~isempty(falseTrig)
+  for j=fliplr(falseTrig)
+    cfg_seg.trl(j, :) = [];
+  end
+end
 
 data    = ft_preprocessing(cfg_seg);
 

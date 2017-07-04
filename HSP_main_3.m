58aac218bcb18e998c9fa0b095af7a1c307e0fc8
Daniel Matthes <dmatthes@cbs.mpg.de>
2017-07-04 14:47:37 +0200

    Funktion HSP_phaseLockVal(…) –  Funktion zur Berechnung des Phase Locking Value vollständig umgesetzt → Funktionalität verifiziert. Skript HSP_main_3 –  Die Durchführung der PLV-Berechnung für 2, 10 und 4  Hz implementiert.

--- HSP_main_3.m
+++ HSP_main_3.m
@@ -12,6 +12,7 @@ else
   error('A dataset with segmented data seems not available.');
 end
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% 2 Hz branch
 %% bandpass filter data at 2Hz
 cfg           = [];
@@ -29,7 +30,6 @@ fprintf('Data stored!\n');
 
 %% calculate hilbert phase at 2Hz
 data_hilbert_2Hz = HSP_hilbertPhase(data_bpfilt_2Hz);
-
 clear data_bpfilt_2Hz
 
 %% export the hilbert phase data into a *.mat file
@@ -39,8 +39,21 @@ fprintf('Saving Hilbert phase data (2Hz) in %s ...\n', file_path);
 save(file_path, 'data_hilbert_2Hz', '-v7.3');
 fprintf('Data stored!\n');
 
+%% calculate PLV at 2 Hz
+cfg           = [];
+cfg.winlen    = 5;                                                          % window length for one PLV value in seconds
+
+data_plv_2Hz = HSP_phaseLockVal(cfg, data_hilbert_2Hz);
 clear data_hilbert_2Hz
 
+%% export the PLVs into a *.mat file
+file_name = strcat(dest_folder, 'HSP_07a_plv2Hz');
+file_path = strcat(file_name, file_version);
+fprintf('Saving PLVs (2Hz) in %s ...\n', file_path);
+save(file_path, 'data_plv_2Hz', '-v7.3');
+fprintf('Data stored!\n');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% 10 Hz branch
 %% bandpass filter data at 10Hz
 cfg           = [];
@@ -58,7 +71,6 @@ fprintf('Data stored!\n');
 
 %% calculate hilbert phase at 10Hz
 data_hilbert_10Hz = HSP_hilbertPhase(data_bpfilt_10Hz);
-
 clear data_bpfilt_10Hz
 
 %% export the hilbert phase data into a *.mat file
@@ -68,8 +80,21 @@ fprintf('Saving Hilbert phase data (10Hz) will be saved in %s ...\n', file_path)
 save(file_path, 'data_hilbert_10Hz', '-v7.3');
 fprintf('Data stored!\n');
 
+%% calculate PLV at 10 Hz
+cfg           = [];
+cfg.winlen    = 1;                                                          % window length for one PLV value in seconds
+
+data_plv_10Hz = HSP_phaseLockVal(cfg, data_hilbert_10Hz);
 clear data_hilbert_10Hz
 
+%% export the PLVs into a *.mat file
+file_name = strcat(dest_folder, 'HSP_07b_plv10Hz');
+file_path = strcat(file_name, file_version);
+fprintf('Saving PLVs (10Hz) in %s ...\n', file_path);
+save(file_path, 'data_plv_10Hz', '-v7.3');
+fprintf('Data stored!\n');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% 40 Hz branch
 %% bandpass filter data at 40Hz
 cfg           = [];
@@ -87,7 +112,6 @@ fprintf('Data stored!\n');
 
 %% calculate hilbert phase at 40Hz
 data_hilbert_40Hz = HSP_hilbertPhase(data_bpfilt_40Hz);
-
 clear data_bpfilt_40Hz
 
 %% export the hilbert phase data into a *.mat file
@@ -97,7 +121,19 @@ fprintf('Saving Hilbert phase data (40Hz) in %s ...\n', file_path);
 save(file_path, 'data_hilbert_40Hz', '-v7.3');
 fprintf('Data stored!\n');
 
+%% calculate PLV at 40 Hz
+cfg           = [];
+cfg.winlen    = 1;                                                          % window length for one PLV value in seconds
+
+data_plv_40Hz = HSP_phaseLockVal(cfg, data_hilbert_40Hz);
 clear data_hilbert_40Hz
+
+%% export the PLVs into a *.mat file
+file_name = strcat(dest_folder, 'HSP_07c_plv40Hz');
+file_path = strcat(file_name, file_version);
+fprintf('Saving PLVs (40Hz) in %s ...\n', file_path);
+save(file_path, 'data_plv_40Hz', '-v7.3');
+fprintf('Data stored!\n');
 
 clear cfg data_seg1 dest_folder file_name file_path file_version ...
       file_num

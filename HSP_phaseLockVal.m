04eda441a51c4a35b69c0ebda30913c0741150f3
Daniel Matthes <dmatthes@cbs.mpg.de>
2017-07-06 14:13:01 +0200

    Funktion HSP_phaseLockVal(...) – Auch noch Zuweisung von dataTmp korrigiert.

--- HSP_phaseLockVal.m
+++ HSP_phaseLockVal.m
@@ -48,13 +48,12 @@ centerFreq = (  data(1).part1.cfg.previous.bpfreq(1) + ...
 % Calculate Phase Locking Value (PLV)
 % -------------------------------------------------------------------------
 dataTmp(numOfPart) = struct;
-dataTmp(numOfPart).part1 = [];
-dataTmp(numOfPart).part2 = [];
+dataTmp(numOfPart).dyad = [];
 
 for i=1:1:numOfPart
   fprintf('Calc PLVs of dyad %d with a center frequency of %d Hz...\n', ...           
             i, centerFreq);
-  dataTmp(i)   = phaseLockingValue(cfg, data(i).part1, data(i).part2);
+  dataTmp(i).dyad  = phaseLockingValue(cfg, data(i).part1, data(i).part2);
 end
 
 data = dataTmp;
